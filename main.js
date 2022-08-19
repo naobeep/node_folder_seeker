bd83ac59eee6935bf51935f09fa4948bbc0fe1c9
naobeep <85398103+naobeep@users.noreply.github.com>
2022-08-19 10:24:57 +0900

    20220819

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import XLSX from 'xlsx';
 import { readFile } from 'fs/promises';
 import { dialog } from './modules/dialog.js';
 
@@ -10,8 +11,15 @@ const json = JSON.parse(
 
 await dialog(settings, json);
 
+const workbook = XLSX.readFile('./assets/blanc.xlsx');
+const sheet1 = workbook.Sheets['Sheet1'];
 const dir = settings.rootDirectory;
 const list = [];
+const startRow = 3;
+const startColumn = 3;
+
+const folderName = dir.split('\\').at(-1);
+sheet1['!ref'] = 'A1:L10000';
 
 const detect = p => {
   fs.readdir(p, (err, files) => {
@@ -23,8 +31,9 @@ const detect = p => {
       if (fs.statSync(fp).isDirectory()) {
         detect(fp);
       } else {
-        const trimStr = fp.replace(dir,'root')
-        list.push(trimStr);
+        const trimStr = fp.replace(dir, 'root');
+        const dirArray = trimStr.split('\\');
+        list.push(dirArray);
       }
     });
   });
@@ -34,17 +43,16 @@ detect(dir);
 
 setTimeout(() => {
   console.log(list);
-  let data = '';
-  for (const item of list) {
-    const arr = item.split('\\');
-    console.log(arr);
-    data += `${arr}\n`;
-  }
-
-  fs.writeFile('./dist/list.txt', data, err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  list.forEach((item, i) => {
+    item.forEach((dir, cellIndex) => {
+      const targetColumn = String.fromCodePoint(cellIndex + 65 + startColumn);
+      sheet1[`${targetColumn}${i + startRow}`] = {
+        t: 's',
+        v: dir,
+        w: dir,
+      };
+    });
   });
+  workbook.Sheets['Sheet1'] = sheet1
+  XLSX.writeFile(workbook,`./dist/${folderName}_directory.xlsx`,{type:'xlsx'})
 }, 3000);
