3139656505760cb40785022a867681707c32eae5
naobeep <85398103+naobeep@users.noreply.github.com>
2022-08-21 12:12:48 +0900

    20220821

--- async.js
+++ async.js
@@ -1,10 +1,30 @@
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 1000, 'foo');
+let a = 0;
+
+const func1 = () => {
+  setTimeout(() => {
+    if (a < 5) {
+      console.log(a);
+      a++;
+      func1();
+    }
+  }, 1000);
+};
+
+func1();
+
+console.log({ a });
+
+const myPromise = new Promise((resolve, reject) => {
+  func1();
+  resolve('done!');
 });
 
-Promise.all([promise1, promise2, promise3]).then(values => {
-  console.log(values);
-});
-// expected output: Arr
+myPromise
+  .then(res => {
+    console.log(res, 'then1', { a });
+    return 'next'
+  })
+  .then(res=>{
+    console.log(res);
+    console.log({a});
+  });
