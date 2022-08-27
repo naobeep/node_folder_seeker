1fa45f99188df94a89fc31f7a0b2c00732b41d66
naobeep <85398103+naobeep@users.noreply.github.com>
2022-08-27 10:15:59 +0900

    20220827

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const settings = {
-  rootDirectory: 'c:\\Users\\naomu\\OneDrive\\デスクトップ\\会議用背景',
-  exclusionString:'(.svn|.vscode)'
-};
-
-const dir = settings.rootDirectory;
-const reg = new RegExp(settings.exclusionString);
-
+const dir = 'c:\\Users\\naomu\\OneDrive\\デスクトップ\\会議用背景';
 const rawFileList = [];
 const rawFolderList = [];
+
+const pathJudge = fp => {
+  return new Promise((resolve, reject) => {
+    if (fs.statSync(fp).isDirectory()) {
+      resolve(fp);
+    } else {
+      reject(fp);
+    }
+  });
+};
 
 // フォルダを探索してリストに書き出す
 const detect = p => {
@@ -18,16 +21,31 @@ const detect = p => {
     if (err) console.error(err);
 
     for (const file of files) {
-      if (file.match(reg)) continue;
+      if (file.match(/(.svn|.vscode)/)) continue;
       const fp = path.join(p, file);
-      if (fs.statSync(fp).isDirectory()) {
-        rawFolderList.push(fp);
-        detect(fp);
-      } else {
-        rawFileList.push(fp);
-      }
+      // console.log({ fp });
+      pathJudge(fp)
+        .then(fp => {
+          // console.log('resolve', fp);
+          rawFolderList.push(fp);
+          detect(fp);
+        })
+        .catch(fp => {
+          // console.log('reject', fp);
+          rawFileList.push(fp);
+        });
     }
   });
 };
 
-detect(dir);
+detect(dir)
+// .then(a => {
+//   console.log('done!', a);
+//   console.log(rawFolderList);
+//   console.log(rawFileList);
+// });
+
+setTimeout(() => {
+  console.log('setTimeout');
+  console.log(rawFolderList);
+}, 1000);
