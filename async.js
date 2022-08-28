a995b43ebab4e006aae121e71395088388644857
naobeep <85398103+naobeep@users.noreply.github.com>
2022-08-29 07:19:14 +0900

    20220829

--- async.js
+++ async.js
@@ -1,49 +1,23 @@
-// let [a, b] = [0, 0];
-// const arr = [];
+let [a, b] = [0, 0];
+const arr = [];
 
-// const func1 = () => {
-//   setTimeout(() => {
-//     console.log({ a });
-//     arr.push(a);
-//     a++;
-//     a < 5 && func1();
-//   }, 1000);
-// };
+const func1 = () => {
+  setTimeout(() => {
+    console.log({ a });
+    arr.push(a);
+    a++;
+    a < 5 && func1();
+  }, 1000);
+};
 
-// const promiseFunc = () => {
-//   return new Promise(resolve => {
-//     b++;
-//     func1();
-//     resolve(b);
-//   });
-// };
+const promiseFunc = () => {
+  return new Promise(resolve => {
+    b++;
+    func1();
+    resolve(b);
+  });
+};
 
-// promiseFunc().then(b => {
-//   console.log({ b }, 'done', { arr });
-// });
-
-var arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-
-
-function recursiveFunc(arr) {
-  while (arr.length > 0) {
-    return new Promise(resolve => {
-      var tmpArr = arr.splice(0, 3);
-      var entryTimestamp = Date.now();
-      setTimeout(function () {
-        logger(tmpArr, entryTimestamp);
-        resolve();
-      }, 1000);
-    }).then(function () {
-      return recursiveFunc(arr);
-    });
-  }
-}
-
-function logger(arr, timestamp) {
-  console.log(JSON.stringify(arr) + timestamp);
-}
-
-recursiveFunc(arr).then(function () {
-  console.log('done');
+promiseFunc().then(b => {
+  console.log({ b }, 'done', { arr });
 });
